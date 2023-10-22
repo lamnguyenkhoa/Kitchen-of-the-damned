f178909d53585ddc19cf951999491be161fc5295
NKLam <khoalamvn@gmail.com>
2023-10-22 19:25:24 +1100

    Add eye corner ghost mechanic and sfx

--- src/Monster/EyeCornerGhost/eye_corner_ghost.gd
+++ src/Monster/EyeCornerGhost/eye_corner_ghost.gd
@@ -5,16 +5,22 @@ extends Monster
 var roam_nodes: Array[Marker3D] = []
 var roam_node_idx = 0
 
+const ANGLE_THRESHOLD = 48
+
 func _ready():
 	# Convert Array[Node] to Array[Marker3D]
 	roam_nodes.assign(roam_node_group.get_children())
 	# Make sure to not await during _ready.
 	call_deferred("actor_setup")
 
-func actor_setup():
-	# Wait for the first physics frame so the NavigationServer can sync.
-	await get_tree().physics_frame
-	navigation_agent.set_target_position(roam_nodes[roam_node_idx].global_position)
+func _process(delta: float) -> void:
+	var monster_dir = global_position - GameManager.player.global_position
+	var look_dir = GameManager.player.get_look_direction()
+	var look_angle = rad_to_deg(monster_dir.angle_to(look_dir))
+	if look_angle < ANGLE_THRESHOLD:
+		visible = false
+	else:
+		visible = true
 
 func _physics_process(delta):
 	if navigation_agent.is_navigation_finished():
@@ -29,6 +35,11 @@ func _physics_process(delta):
 	var new_velocity = (next_path_position - current_agent_position).normalized() * move_speed
 	velocity = new_velocity
 	move_and_slide()
+
+func actor_setup():
+	# Wait for the first physics frame so the NavigationServer can sync.
+	await get_tree().physics_frame
+	navigation_agent.set_target_position(roam_nodes[roam_node_idx].global_position)
 
 
 
